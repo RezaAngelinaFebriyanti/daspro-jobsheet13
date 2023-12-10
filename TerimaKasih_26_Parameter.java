9357db103f75ffb98ca71a681712ac877951df81
Reza Angelina Febriyanti <rzangef15@gmail.com>
2023-12-10 21:05:20 +0700

    commit

---
+++ TerimaKasih_26_Parameter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TerimaKasih_26_Parameter {
+    public static void UcapanTerimaKasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+    public static void main(String[] args) {
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTerimaKasih();
+        UcapanTambahan(ucapan);
+    }
+}
