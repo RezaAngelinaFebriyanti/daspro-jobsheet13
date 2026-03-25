f63c833c72977933788dc4d5fe7075fc34694360
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2026-03-25 22:42:33 +0700

    Percobaan 2: Membuat Fungsi Dengan Parameter

--- TerimaKasih_26_Parameter.java
+++ TerimaKasih_26_Parameter.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class TerimaKasih_26_Parameter {
     public static void UcapanTerimaKasih() {
         System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
