9d897f505f925baa81166afe1d546bfead6ef97c
RezaAngelinaFebriyanti <rzangef15@gmail.com>
2026-03-25 22:45:57 +0700

    Modif Percobaan 1 Tanpa Menggunakan Fungsi UcapanTerimaKasih

--- TerimaKasih_26_NoFungsi.java
+++ TerimaKasih_26_NoFungsi.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class TerimaKasih_26_NoFungsi {
     public static void main(String[] args) {
         System.out.println("Thank you for being the best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like I could ask you anything.");
